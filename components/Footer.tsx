7775eaf158d561c1e0774399edbf9473cd733456
AvianIndustries <62577392+AvianIndustries@users.noreply.github.com>
2025-09-21 09:36:57 -0500

    AI generated: components/Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className='p-5 mt-5 bg-yellow-500 text-white'>
+      <p>&copy; 2022 Peanut Butter World. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
