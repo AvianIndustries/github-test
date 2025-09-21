9c8396d579a7e63bc2be278f8552f0aabb688939
AvianIndustries <62577392+AvianIndustries@users.noreply.github.com>
2025-09-21 09:36:57 -0500

    AI generated: components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <header className='p-5 bg-yellow-500 text-white'>
+      <h1 className='text-3xl font-bold'>Peanut Butter World</h1>
+    </header>
+  );
+};
+
+export default Header;
